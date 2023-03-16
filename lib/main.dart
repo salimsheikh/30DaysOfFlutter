63eaaaaa863137742f91023366bbcb3a3717ae7a
Salim Shaikh <salimsheikh4u2000@gmail.com>
2023-03-16 15:37:19 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Container(
+          child: Text("Hello World! My First Flutter App"),
+        ),
+      ),
+    );
+  }
+}
