fe7594076d4a3503ca04330dd8267231aa2c6a43
Salim Shaikh <salimsheikh4u2000@gmail.com>
2023-03-17 18:13:48 +0530

    Day 5 Complted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utlis/routes.dart';
 import 'pages/homepage.dart';
 import 'pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -10,15 +12,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       //home: LoginPage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        //primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
-        "/": (context) => HomePage(),
-        "/home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,8 +1,55 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import '../utlis/routes.dart';
 
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset("assets/images/login.png", fit: BoxFit.cover),
+          SizedBox(height: 20.0),
+          Text(
+            "Welcome",
+            style: TextStyle(
+                fontSize: 28, color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20.0),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter Username", labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password", labelText: "Password"),
+                ),
+                SizedBox(height: 40.0),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  onPressed: () {
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+/* Day 3 tutoiral */
+class DayThreeLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
