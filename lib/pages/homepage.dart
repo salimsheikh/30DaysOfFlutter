bd1dd980cc4263786978364c2e2c45d129e672e4
Salim Shaikh <salimsheikh4u2000@gmail.com>
2023-03-18 20:49:16 +0530

    Day 12 Started

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: must_be_immutable
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import '../widgets/mydrawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -10,14 +12,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Catalog Page"),
+        title: Text("Catalog App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
       ),
       drawer: const MyDrawer(),
-      body: Center(
-        child: Text("Welcom to $days days of flutter by $name"),
-      ),
     );
   }
 }
