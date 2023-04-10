1d28adb620dfadc466bf59f888c999af7a30e5a1
Salim Shaikh <salimsheikh4u2000@gmail.com>
2023-04-10 12:59:59 +0530

    Day 30.1 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+//import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter_catalog/models/cart.dart';
 
 import 'package:flutter_catalog/models/catalog.dart';
@@ -25,6 +26,9 @@ class _HomePageState extends State<HomePage> {
 
   String name = "Salim Shaikh";
 
+  //final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
+  final url = "http://demos.infosofttech.com/flutter-api/catalog.json";
+
   @override
   void initState() {
     super.initState();
@@ -32,8 +36,11 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    //await Future.delayed(Duration(seconds: 5));
-    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    //var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+
+    final response = await http.get(Uri.parse(url));
+    final catalogJson = response.body;
+
     var decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
 
@@ -60,7 +67,7 @@ class _HomePageState extends State<HomePage> {
             CupertinoIcons.cart,
             color: Colors.white,
           ),
-        ).badge(color: Colors.red, size: 20, count: cartData.items.length),
+        ).badge(color: Vx.red500, size: 20, count: cartData.items.length),
       ),
       body: SafeArea(
         child: Container(
